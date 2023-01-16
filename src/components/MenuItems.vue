2f38a5176bf9b055e89a2553828ada81a1433bb9
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-01-17 01:26:43 +0530

    final commit

--- src/components/MenuItems.vue
+++ src/components/MenuItems.vue
@@ -11,14 +11,14 @@ export default {
 <template>
   <div
     :class="[
-      'flex space-x-2 items-center rounded-full px-2 py-2 w-full hover:bg-cyan-100',
+      'flex space-x-2 my-1 items-center rounded-full px-2 py-2 w-full hover:bg-cyan-100',
       {
         'bg-cyan-50 font-semibold': active,
       },
     ]"
   >
     <component :is="icon" v-if="icon" class="h-5 w-5" />
-    <button class="rounded-md text-left">
+    <button class="rounded-md text-left text-sm">
       {{ name }}
     </button>
   </div>
